c2f659ff6304e13f588eec37ecaf34fa33df07fc
Hruday Kolipaka <146696272+Hruday-Kolipaka@users.noreply.github.com>
2024-08-16 11:04:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "hruday-bucket"
+  default     = "hruday-kolipaka-bucket"
 }
 
 variable "acl" {
