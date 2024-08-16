cf97425352bf88a2f07e141ad8ad63f56b6b8710
Hruday Kolipaka <146696272+Hruday-Kolipaka@users.noreply.github.com>
2024-08-16 12:08:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hruday-kolipaka-bucket"
+    bucket         = "mybucket"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "hruday-dynamo-db-table"
